b0d8af0a275f1ded49aeaebf7d3a83dc3e57c802
Riley Shaw <rileyjshaw@gmail.com>
2017-03-26 05:08:18 -0400

    Add WarGames to hackers.cool

--- hackers.cool/js/index.js
+++ hackers.cool/js/index.js
@@ -23,34 +23,98 @@
       return 'redirecting...';
     }
 
-    function type (str, speed, cb) {
+    function type (mode, strs, options) {
+      var cb, ignoreCommand, speed;
+
+      if (typeof options === 'function') {
+        cb = options;
+      } else {
+        options = options || {};
+        cb = options.cb;
+        ignoreCommand = options.ignoreCommand;
+        speed = options.speed;
+      }
+
+      if (typeof strs === 'string') {
+        strs = [strs];
+      }
+
+      if (typeof speed !== 'number') {
+        speed = mode === 'term' ? 50 : 300;
+      }
+
+      strs = strs.reverse();
+
       // safety measures
       term.stopLoading(true);
       term.input.disabled = true;
 
-      if (typeof speed !== 'number') {
-        speed = 150;
+      if (mode === 'term') {
+        term.form.style.display = 'none';
       }
 
-      str = str.split('').reverse();
-      str.push('');
+      !function typeLine () {
+        var str, el, inner;
 
-      !function inner () {
-        var delay = (0.25 + Math.random()) * speed;
-        term.input.value += str.pop();
+        str = strs.pop().split('').reverse();
+        str.push('');
 
-        if (str.length) {
-          setTimeout(inner, delay);
-        } else {
-          setTimeout(function () {
-            term.run(function () {
-              term.cmdHistory.pop();
+        // Update the scroll position once the first character's in there.
+        setTimeout(function () { term.print(); }, 1.5 * speed);
+
+        if (mode === 'term') {
+          el = document.createElement('code');
+          term.container.insertBefore(el, term.form);
+
+          inner = function inner () {
+            var delay = (0.25 + Math.random()) * speed;
+            el.textContent += str.pop();
+
+            if (str.length) {
+              setTimeout(inner, delay);
+            } else if (strs.length) {
+              typeLine();
+            } else {
+              term.input.disabled = false;
+              term.form.reset();
+              term.form.style.display = 'block';
+              term.print();
+
               if (typeof cb === 'function') {
                 cb();
               }
-            });
-          }, delay);
+            }
+          };
+        } else {
+          inner = function inner () {
+            var delay = (0.25 + Math.random()) * speed;
+            term.input.value += str.pop();
+
+            if (str.length) {
+              setTimeout(inner, delay);
+            } else if (strs.length) {
+              typeLine();
+            } else if (!ignoreCommand) {
+              setTimeout(function () {
+                term.run(function () {
+                  term.cmdHistory.pop();
+                  if (typeof cb === 'function') {
+                    cb();
+                  }
+                });
+              }, delay);
+            } else {
+              term.print('>&nbsp;' + term.input.value);
+              term.form.reset();
+
+              if (typeof cb === 'function') {
+                cb();
+              }
+            }
+          };
         }
+
+        inner();
       }();
     }
 
@@ -93,7 +157,10 @@
 
         !function next () {
           if (lyrics.length) {
-            type('echo ' + lyrics.pop(), 100, next);
+            type('user', 'echo ' + lyrics.pop(), {
+              cb: next,
+              speed: 100,
+            });
           } else {
             redirect('http://youtu.be/dQw4w9WgXcQ', true);
           }
@@ -111,6 +178,62 @@
       desc: 'Says pong.'
     });
 
+    term.register('wargames', {
+      fn: function () {
+        var resetRun = (function () {
+          oldRun = term.run;
+          return function resetRun () { term.run = oldRun; };
+        })();
+
+        term.run = function () {
+          var cmd = term.form.prompt.value;
+
+          term.form.reset();
+          term.print('>&nbsp;' + cmd);
+          cmd = cmd.toLowerCase();
+
+          if (cmd === 'joshua') {
+            type('term', 'GREETINGS PROFESSOR FALKEN', function () {
+              term.run = function () {
+                term.print('>&nbsp;' + term.form.prompt.value);
+
+                type('term', [
+                  'A STRANGE GAME',
+                  'THE ONLY WINNING MOVE IS',
+                  'NOT TO PLAY.',
+                ], function () {
+                  term.print('&nbsp;');
+                  type('term', 'HOW ABOUT A NICE GAME OF CHESS?', function () {
+                    term.register('chess', {
+                      fn: function () {
+                        redirect('http://www.imdb.com/title/tt0086567/', true);
+                      },
+                      desc: 'The hottest new game from PROTOVISION',
+                    });
+                    resetRun();
+                  });
+                });
+              };
+              type('user', 'HELLO');
+            });
+          } else if (cmd === 'help' || cmd === 'help logon') {
+            return type('term', 'HELP NOT AVAILABLE', function () {
+              term.print('&nbsp;');
+              type('term', 'LOGON:');
+            });
+          } else {
+            type('term', [
+              'IDENTIFICATION NOT RECOGNIZED BY SYSTEM',
+              '--CONNECTION TERMINATED--',
+            ], resetRun);
+          }
+        };
+
+        return 'LOGON:';
+      },
+      desc: ' ===> (311) 437-8739',
+    });
+
     term.register('webring', {
       args: ['prev', 'next'],
       fn: function (direction) {
@@ -126,16 +249,16 @@
       aliases: ['ring']
     });
 
-    term.register('welcome', {
+    term.register('whoami', {
       fn: function () {
         return 'You are visitor #' + phpDataRJS.visNum + ' (or more).';
       },
       desc: 'Prints visitor number.'
     });
 
-    window.setTimeout(function () {
-      type('welcome');
-    }, 0);
+    type('term', 'Welcome', function () {
+      type('user', 'whoami');
+    });
   }
 
   if (typeof WebFont === 'object') {
